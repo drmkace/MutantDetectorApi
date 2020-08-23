5c2b0c84333b1bad1a35bc3497718b17e65b628b
drmkace <drmkace@yahoo.com.ar>
2020-08-23 16:46:00 -0300

    Add Swagger UI

--- adn/src/main/java/com/magneto/adn/controller/MutantDetectorController.java
+++ adn/src/main/java/com/magneto/adn/controller/MutantDetectorController.java
@@ -5,10 +5,12 @@ import com.magneto.adn.dto.PingResponseDto;
 import com.magneto.adn.exception.InvalidDnaException;
 import com.magneto.adn.service.MutantDetectorService;
 import com.magneto.adn.util.DbSchemaBuilder;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 public class MutantDetectorController {
@@ -26,6 +28,7 @@ public class MutantDetectorController {
 
     @GetMapping("/ping")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value =  "Pings for Api and DB Statuses", notes = "Verifies if the APi and Database are Alive ")
     public PingResponseDto ping() {
         var pingDatabase = dbschemaBuilder.ping();
         PingResponseDto responseDto = new PingResponseDto("OK", pingDatabase ? "OK" : "N/A");
@@ -33,6 +36,7 @@ public class MutantDetectorController {
     }
 
     @PostMapping("/mutant")
+    @ApiOperation(value =  "Detects if the Dna is Mutant or Not", notes = "Returns 200 - OK if Its Mutant 403 - Forbidden if not")
     public ResponseEntity<String> detectMutant(@RequestBody DnaSampleRequestDto dnaSampleRequestDto) {
         if(dnaSampleRequestDto == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
--- adn/src/main/java/com/magneto/adn/controller/StatController.java
+++ adn/src/main/java/com/magneto/adn/controller/StatController.java
@@ -2,10 +2,12 @@ package com.magneto.adn.controller;
 
 import com.magneto.adn.dto.StatResponseDto;
 import com.magneto.adn.service.StatService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 public class StatController {
@@ -17,6 +19,7 @@ public class StatController {
     }
 
     @GetMapping("/stats")
+    @ApiOperation(value =  "Gets Mutant Detector Stats", notes = "Gets Human and Mutant Count and Ratio")
     public ResponseEntity<StatResponseDto> getStat() {
         var stat = service.get();
         var statResponseDto = new StatResponseDto(stat.getHumanCount(), stat.getMutantCount());
